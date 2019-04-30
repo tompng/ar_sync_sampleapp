c3d66e35f700813ebd4d3e7a67a95d1be3953427
tompng <tomoyapenguin@gmail.com>
2019-04-30 14:38:18 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'top#show'
+  get '/react_top', to: 'top#react'
+
+  resources :posts, except: :index do
+    member do
+      post :reaction
+    end
+  end
+  resources :users, only: %i[index show]
+
+  resources :comments, except: %i[new index] do
+    member { post :reaction }
+  end
+
+  get '/followings', to: 'follows#followings'
+  get '/followeds', to: 'follows#followees'
+  resources :follows, only: :index do
+    collection do
+      post :follow
+      post :unfollow
+    end
+  end
+
+  get '/user/sign_in', to: 'sessions#new', as: :sign_in
+  post '/user/sign_in', to: 'sessions#create'
+  get '/user/sign_out', to: 'sessions#destroy', as: :sign_out
 end
