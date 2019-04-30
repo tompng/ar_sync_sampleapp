f4b3ea6746ffe192aa491836bf0ce09d8d462ce6
tompng <tomoyapenguin@gmail.com>
2019-04-30 14:30:23 +0900

    add controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  def authenticate_user!
+    redirect_to sign_in_path unless current_user
+  end
+
+  helper_method def user_signed_in?
+    current_user.present?
+  end
+
+  helper_method def current_user
+    @current_user ||= User.find_by id: session[:id]
+  end
 end
