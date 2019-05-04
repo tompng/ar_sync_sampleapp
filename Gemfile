349b101522456aaa33cb0229ea44659e3393f709
tompng <tomoyapenguin@gmail.com>
2019-05-04 21:24:44 +0900

    set graphiql (optional)

--- Gemfile
+++ Gemfile
@@ -62,3 +62,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'ar_sync'
+gem 'graphiql-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   post '/sync_api', to: 'sync_api#sync_call'
   post '/static_api', to: 'sync_api#static_call'
   post '/graphql', to: 'sync_api#graphql_call'
+
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'top#show'
   get '/react_top', to: 'top#react'
